8a376c0eec0e6ec16bee5742874e9668da23dd4e
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-16 16:46:47 -0300

    Adiciona a regra de negocio para listar as tasks.

--- back-end/src/msc-layers/routers/tasks.router.js
+++ back-end/src/msc-layers/routers/tasks.router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const authMiddleware = require('../../middlewares/authMiddleware');
-const { registerTaskController } = require('../controllers/tasks.controller');
+const {
+  registerTaskController,
+  getAllUserTasksController,
+} = require('../controllers/tasks.controller');
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.post('/', registerTaskController);
+
+router.get('/', getAllUserTasksController);
 
 module.exports = router;
