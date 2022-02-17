edc6f1c5302f8c1eb8bf596c2a30b0c51800b0c2
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-17 02:10:06 -0300

    Adiciona a pagina inicial.

---
+++ front-end/src/pages/LoginRegister.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from '../components/Login.jsx';
+import Register from '../components/Register.jsx';
+
+function LoginRegister() {
+  return (
+    <main>
+      <Login/>
+      <br />
+      <br />
+      <br />
+      <Register />
+    </main>
+  );
+}
+
+export default LoginRegister;
