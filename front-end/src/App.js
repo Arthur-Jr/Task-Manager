79ee79ef4f0d4b8442d444b776ff0eb4b45a8d7e
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-17 02:13:45 -0300

    Adiciona a pagina de tasks no Route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './CSS/App.css';
 import LoginRegister from './pages/LoginRegister.jsx';
+import Tasks from './pages/Tasks.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={ <LoginRegister/> } />
+      <Route path='/tasks' element= { <Tasks/> }/>
     </Routes>
   );
 }
