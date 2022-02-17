12f4546919bcb2bd71800e6044ddeb72a9333bb4
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-16 23:01:35 -0300

    Iniciando o front-end.

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import './CSS/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
