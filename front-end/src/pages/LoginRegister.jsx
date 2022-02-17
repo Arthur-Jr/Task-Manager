1236bc4e1179c1c88d80e5a87a57f5ca3d796494
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-17 04:46:58 -0300

    Refatora a pagina Inicial

--- front-end/src/pages/LoginRegister.jsx
+++ front-end/src/pages/LoginRegister.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
 import Login from '../components/Login.jsx';
 import Register from '../components/Register.jsx';
+import api from '../api/api';
 
 function LoginRegister() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('task-manager-token');
+    const getTasks = async () => {
+      try {
+        const response = await api.get('/tasks', { headers: { authorization: token } });
+        if (response.status === 200) navigate('/tasks');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (token) {
+      getTasks();
+    }
+  }, [navigate]);
+
   return (
     <main>
       <Login/>
