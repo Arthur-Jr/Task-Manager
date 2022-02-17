eb448d3d40de05b220df8c9f3faee402b98e5080
Arthur-Jr <arthurjr1906@gmail.com>
2022-02-17 00:45:35 -0300

    Refatora a api no front

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-require('dotenv').config();
-
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3001',
+  baseURL: 'http://localhost:3001',
 });
 
 export default api;
