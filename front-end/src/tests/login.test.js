854c21e910d33003083e9f8db7769f4bf2549d72
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-23 22:04:05 -0300

    fix: Muda o nome do arquivo de testes do login

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -17,9 +17,40 @@ afterAll(() => {
 });
 
 describe('Testes do login.', () => {
-  global.innerWidth = 650;
+  describe('Testando se os elementos estão presentes na tela no desktop.', () => {
+    beforeEach(() => {
+      renderWithRouter(<App />);
+    });
 
-  describe('Testando se os elementos estão presentes na tela.', () => {
+    test('Deve conter dois títulos na tela "Login" e "Cadastro" ', () => {
+      const titles = screen.getAllByRole('heading', { level: 3 });
+      expect(titles[0]).toBe('Login');
+      expect(titles[1]).toBe('Cadastro');
+    });
+
+    test('Deve conter cinco inputs na tela.', () => {
+      const emails = screen.getByAllPlaceholderText(/email/i);
+      const passwords = screen.getAllByPlaceholderText(/password/i);
+      const name = screen.getByPlaceholderText(/nome/i);
+
+      expect(emails).toHaveLength(2);
+      expect(passwords).toHaveLength(2);
+      expect(name).toBeInTheDocument();
+    });
+
+    test('Deve conter dois botões na tela', () => {
+      const buttons = screen.getAllByRole('button');
+
+      expect(buttons).toHaveLength(2);
+      expect(buttons[0]).toBeInTheDocument();
+      expect(buttons[0].innerText).toBe('Login');
+      expect(buttons[1]).toBeInTheDocument();
+      expect(buttons[1].innerText).toBe('Cadastrar');
+    });
+  });
+
+  global.innerWidth = 650;
+  describe('Testando se os elementos estão presentes na tela no mobile.', () => {
     beforeEach(() => {
       renderWithRouter(<App />);
     });
@@ -30,15 +61,21 @@ describe('Testes do login.', () => {
     });
 
     test('Deve conter dois inputs na tela um de email e outro de password.', () => {
-      const email = screen.getByPlaceholderText('email');
-      const password = screen.getByPlaceholderText('password');
+      const email = screen.getByPlaceholderText(/email/i);
+      const password = screen.getByPlaceholderText(/password/i);
+
       expect(email).toBeInTheDocument();
       expect(password).toBeInTheDocument();
     });
 
-    test('Deve conter um botão na tela', () => {
+    test('Deve conter um botão na tela de entrar', () => {
       const button = screen.getByRole('button', { name: /entrar/i });
       expect(button).toBeInTheDocument();
+    });
+
+    test('Deve conter o botão para ir pra parte de cadastro', () => {
+      const registerButton = screen.getByRole('button', { name: /cadastrar-se/i });
+      expect(registerButton).toBeInTheDocument();
     });
 
     test('A parte de cadastro não deve estar presente na tela ao abrir a página.', () => {
@@ -55,8 +92,8 @@ describe('Testes do login.', () => {
     });
 
     test('Deve apresentar uma menssagem de erro ao colocar um email inválido.', () => {
-      const email = screen.getByPlaceholderText('email');
-      const password = screen.getByPlaceholderText('password');
+      const email = screen.getByPlaceholderText(/email/i);
+      const password = screen.getByPlaceholderText(/password/i);
       const button = screen.getByRole('button', { name: /entrar/i });
 
       userEvent.type(email, 'xabalu');
@@ -73,8 +110,8 @@ describe('Testes do login.', () => {
     });
 
     test('Deve apresentar uma menssagem de erro ao colocar uma senha inválida.', () => {
-      const email = screen.getByPlaceholderText('email');
-      const password = screen.getByPlaceholderText('password');
+      const email = screen.getByPlaceholderText(/email/i);
+      const password = screen.getByPlaceholderText(/password/i);
       const button = screen.getByRole('button', { name: /entrar/i });
 
       userEvent.type(email, 'email@email.com');
@@ -92,8 +129,8 @@ describe('Testes do login.', () => {
 
     test('Deve apresentar uma menssagem de erro ao colocar dados incorreto."', () => {
       axiosPostMock({ data: { message: 'Incorrect username or password' } });
-      const email = screen.getByPlaceholderText('email');
-      const password = screen.getByPlaceholderText('password');
+      const email = screen.getByPlaceholderText(/email/i);
+      const password = screen.getByPlaceholderText(/password/i);
       const button = screen.getByRole('button', { name: /entrar/i });
 
       userEvent.type(email, 'email@email.com');
@@ -111,8 +148,8 @@ describe('Testes do login.', () => {
 
     test('Deve ir para a página de "tasks se os dados estiverem corretos."', () => {
       axiosPostMock({ data: { token: 'token' } });
-      const email = screen.getByPlaceholderText('email');
-      const password = screen.getByPlaceholderText('password');
+      const email = screen.getByPlaceholderText(/email/i);
+      const password = screen.getByPlaceholderText(/password/i);
       const button = screen.getByRole('button', { name: /entrar/i });
 
       userEvent.type(email, 'email@email.com');
